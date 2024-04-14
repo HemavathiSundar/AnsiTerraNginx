165f204f7c6c5333717da29a3819329b9ecdebce
HemavathiSundar <148438501+HemavathiSundar@users.noreply.github.com>
2024-04-14 21:29:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "nginx_instance" {
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "nginx-instance"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i '${self.public_ip},' playbook.yml"
+  }
+}
